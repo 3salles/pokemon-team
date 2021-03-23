b9dab5739e32fa157fcf6bcd1cccec78617c2026
Beatriz <beatrizsallesss@gmail.com>
2021-03-23 18:40:08 -0300

    ✨ feat: Add create table query.

---
+++ src/db/CreateTable.java
@@ -0,0 +1,55 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class CreateTable {
+	
+	public static void main(String[] args) {
+		
+		String sql = "CREATE TABLE pokemons "+
+					 "( official_id integer NOT NULL, " +
+					 "name character varying(20) NOT NULL, " +
+					 "primary_type character varying(20) NOT NULL, " +
+					 "second_type character varying(20) NULL, " +
+					 "atk integer NOT NULL, " +
+					 "def integer NOT NULL, " +
+					 "hp integer NOT NULL, " +
+					 "speed integer NOT NULL, " +
+					 "CONSTRAINT pokemon_pkey PRIMARY KEY (official_id))";
+
+		
+		Connection connection = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			
+			connection = JdbcConnection.getConnection();
+			pstm = connection.prepareStatement(sql);
+			pstm.execute();
+			
+			System.out.println("Tabela criada!");
+			
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			
+		} finally {
+			
+			try {
+				if(pstm != null) {
+					pstm.close();
+				}
+				
+				if(connection !=null) {
+					connection.close();
+				}
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
+		
+	}
+
+	
+	
+}

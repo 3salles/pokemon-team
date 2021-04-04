d1fb3c2780032d0a965a067ad9d3f49e14566e8d
Beatriz Salles <beatrizsallesss@gmail.com>
2021-04-04 15:39:37 -0300

    🚚 build: Move project to Netbeans IDE.

--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -1,89 +1,101 @@
 package model;
 
 public class Pokemon {
-	
-	private int official_id;
-	private String name;
-	private String primary_type;
-	private String second_type;
-	private int atk;
-	private int def;
-	private int hp;
-	private int speed;
-	
-	public Pokemon () {}
-	
-	public Pokemon (
-			int official_id, 
-			String name, 
-			String primary_type, 
-			String second_type, 
-			int atk,
-			int def,
-			int hp,
-			int speed
-			) {
-		this.official_id = official_id;
-		this.name = name;
-		this.primary_type = primary_type;
-		this.second_type = second_type;
-		this.atk = atk;
-		this.def = def;
-		this.hp = hp;
-		this.speed = speed;
-	}
-	
 
-	public int getOfficial_id() {
-		return official_id;
-	}
-	public void setOfficial_id(int official_id) {
-		this.official_id = official_id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPrimary_type() {
-		return primary_type;
-	}
-	public void setPrimary_type(String primary_type) {
-		this.primary_type = primary_type;
-	}
-	public String getSecond_type() {
-		return second_type;
-	}
-	public void setSecond_type(String second_type) {
-		this.second_type = second_type;
-	}
-	public int getAtk() {
-		return atk;
-	}
-	public void setAtk(int atk) {
-		this.atk = atk;
-	}
-	public int getDef() {
-		return def;
-	}
-	public void setDef(int def) {
-		this.def = def;
-	}
-	public int getHp() {
-		return hp;
-	}
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-	public int getSpeed() {
-		return speed;
-	}
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-	
-	
+    private int official_id;
+    private String name;
+    private String primary_type;
+    private String second_type;
+    private int atk;
+    private int def;
+    private int hp;
+    private int speed;
 
+    public Pokemon() {
+    }
+
+    public Pokemon(
+            int official_id,
+            String name,
+            String primary_type,
+            String second_type,
+            int atk,
+            int def,
+            int hp,
+            int speed
+    ) {
+        this.official_id = official_id;
+        this.name = name;
+        this.primary_type = primary_type;
+        this.second_type = second_type;
+        this.atk = atk;
+        this.def = def;
+        this.hp = hp;
+        this.speed = speed;
+    }
+
+    public int getOfficial_id() {
+        return official_id;
+    }
+
+    public void setOfficial_id(int official_id) {
+        this.official_id = official_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrimary_type() {
+        return primary_type;
+    }
+
+    public void setPrimary_type(String primary_type) {
+        this.primary_type = primary_type;
+    }
+
+    public String getSecond_type() {
+        return second_type;
+    }
+
+    public void setSecond_type(String second_type) {
+        this.second_type = second_type;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -4,38 +4,34 @@ import dao.PokemonDAO;
 import model.Pokemon;
 
 public class Main {
-	public static void main(String[] args) {
-		PokemonDAO pokemonDao = new PokemonDAO();
 
-		/*
-		 * Pokemon vulpix = new Pokemon(37, "Vulpix Alola","Gelo", null, 41, 40, 38,
-		 * 65); pokemonDao.savePokemon(vulpix);
-		 * 
-		 * Pokemon buizel = new Pokemon(330, "Buizel","Água", null, 65, 35, 55, 85);
-		 * pokemonDao.savePokemon(buizel);
-		 * 
-		 * Pokemon growlithe = new Pokemon(58, "Growlithe", "Fogo", null, 70, 45, 55,
-		 * 60); pokemonDao.savePokemon(growlithe);
-		 * 
-		 * pokemonDao.savePokemon(squirtle);
-		 */
-		Pokemon squirtle = new Pokemon(7, "Squirtle", "Água", null, 48, 65, 44, 43);
-		
+    public static void main(String[] args) {
+        PokemonDAO pokemonDao = new PokemonDAO();
 
-		// List Pokemons
-		System.out.println("ID - Pokemon - HP - ATK - DEF - SPEED");
+        
+		 /* Pokemon vulpix = new Pokemon(37, "Vulpix Alola","Gelo", null, 41, 40, 38,
+		  65); pokemonDao.savePokemon(vulpix);
+		  
+		  Pokemon buizel = new Pokemon(330, "Buizel","Água", null, 65, 35, 55, 85);
+		 pokemonDao.savePokemon(buizel);
+		 
+		 Pokemon growlithe = new Pokemon(58, "Growlithe", "Fogo", null, 70, 45, 55,
+		 60); pokemonDao.savePokemon(growlithe);
+		
+		pokemonDao.savePokemon(squirtle);
+        
+        Pokemon squirtle = new Pokemon(7, "Squirtle", "Água", null, 48, 65, 44, 43); */
 
-		for (Pokemon p : pokemonDao.getPokemons()) {
-			System.out.printf("%d - %s - %d - %d - %d - %d\n", p.getOfficial_id(), p.getName(), p.getHp(), p.getAtk(),
-					p.getDef(), p.getSpeed());
-		}
-		
-		// Delete Pokemon
-		
-		
-		pokemonDao.deletePokemon(squirtle);
-		
-		
+        // List Pokemons
+        System.out.println("ID - Pokemon - HP - ATK - DEF - SPEED");
 
-	}
+        for (Pokemon p : pokemonDao.getPokemons()) {
+            System.out.printf("%d - %s - %d - %d - %d - %d\n", p.getOfficial_id(), p.getName(), p.getHp(), p.getAtk(),
+                    p.getDef(), p.getSpeed());
+        }
+
+        // Delete Pokemon
+        /*pokemonDao.deletePokemon(squirtle);*/
+
+    }
 }
